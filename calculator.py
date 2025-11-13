e5e72199e084b447573ec47fd7ed349adac3a706
hannahpjohns <hp.johns@ufl.edu>
2025-11-12 19:42:06 -0500

    comments

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+# https://github.com/hannahpjohns/lab11-HJ-KR.git
+# Partner 1: Hannah Johns
+# Partner 2: Kaydence Rao
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,6 +8,15 @@ calculator.py
 One function per operation, in order.
 """
 import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 # First example
 def add(a, b):
